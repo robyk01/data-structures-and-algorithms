77b00ef26055e69b3e464d77ecd52f3b57cdbb43
Roberto <amoreroberto85@gmail.com>
2025-11-05 01:17:01 +0200

    Corrected Stack paranthesis problem

--- stack/paranthesis.cpp
+++ stack/paranthesis.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
+#include <cstring>
+#include <fstream>
 using namespace std;
+
+ifstream fin("paranteze1.in");
+ofstream fout("paranteze1.out");
 
 char st[1001];
 int vf = 0;
@@ -20,26 +25,29 @@ void top(){
     }
 }
 
-int main(){
-    int n;
-    cin >> n, cin.get();
-    while (n){
-        char p[256];
-        cin >> p;
-        for (int i = 0; i < strlen(p); i++){
-            push(p[i]);
-        }
-
-        int ok = 0;
-        while (vf){
-            if (st[vf - 1] == ')') ok++;
-            else ok--;
-
+bool check_paranthesis(char *p){
+    vf = 0;
+    int len = strlen(p);
+    for (int i = 0; i < len; i++){
+        if (p[i] == '(') push(p[i]);
+        else {
+            if (vf == 0) return false;
             pop();
         }
+    }
 
-        if (!ok) cout << 1 << endl;
-            else cout << 0 << endl;
+    return vf == 0;
+}
+
+int main(){
+    int n;
+    fin >> n, fin.get();
+    while (n){
+        char p[256];
+        fin >> p;
+       
+        if (check_paranthesis(p)) fout << 1 << endl;
+        else fout << 0 << endl;
         n--;
     }
 }
