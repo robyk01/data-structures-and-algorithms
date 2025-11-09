4a0ded671624a577502b571132fcac667ebf2181
Roberto <amoreroberto85@gmail.com>
2025-11-09 19:30:33 +0200

    Added comments

--- stack/stack.cpp
+++ stack/stack.cpp
@@ -4,16 +4,25 @@ using namespace std;
 
 int st[1001], vf = 0;
 
+/*
+    Push a character onto the stack
+*/
 void push(int x){
     st[vf++] = x;
 }
 
+/* 
+    Pop the top element from the stack if it's not empty
+*/
 void pop(){
     if (vf){
         vf--;
     }
 }
 
+/*
+    Print the top element of the stack
+*/
 void top(){
     if (vf){
         cout << st[vf - 1] << endl;
